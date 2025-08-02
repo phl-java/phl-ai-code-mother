83c03b46b4947d79919153c31b1dc34ee4a7b193
裴恒林 <19711007989@163.com>
2025-08-02 20:51:35 +0800

    cs

--- phl-ai-code-frontend/src/components/HelloWorld.vue
+++ phl-ai-code-frontend/src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ defineProps<{
     <h3>
       You’ve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 33333</a>. What's next?
     </h3>
   </div>
 </template>
