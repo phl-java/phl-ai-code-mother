9aaea6c816595d55ade8278999adcef36680d67a
裴恒林 <19711007989@163.com>
2025-08-02 19:31:07 +0800

    后端项目初始化

---
+++ src/main/java/com/example/phlaicodemother/PhlAiCodeMotherApplication.java
@@ -0,0 +1,13 @@
+package com.example.phlaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhlAiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhlAiCodeMotherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/phlaicodemother/PhlAiCodeMotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phlaicodemother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhlAiCodeMotherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
